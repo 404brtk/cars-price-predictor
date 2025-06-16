dc427525ede1a1a873a616153a7c1d380bd0dcc5
Bartosz Zielonka <bzielonka1@proton.me>
2025-06-17 00:44:31 +0200

    setup admin

--- backend/predictor/admin.py
+++ backend/predictor/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Prediction
 
-# Register your models here.
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'car_model', 'year_of_production', 'predicted_price', 'user', 'timestamp')
+    list_filter = ('brand', 'fuel_type', 'transmission', 'body', 'number_of_doors', 'color')
+    search_fields = ('brand', 'car_model', 'user__username')
+    readonly_fields = ('timestamp',)
+    list_per_page = 25
+    
+    fieldsets = (
+        ('Car Information', {
+            'fields': ('brand', 'car_model', 'year_of_production', 'mileage', 'engine_capacity', 'power')
+        }),
+        ('Specifications', {
+            'fields': ('fuel_type', 'transmission', 'body', 'number_of_doors', 'color')
+        }),
+        ('Prediction Data', {
+            'fields': ('predicted_price', 'user', 'timestamp')
+        }),
+    )
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+# register the admin class with the associated model
+admin.site.register(Prediction, PredictionAdmin)
+
+# unregister the default User admin and register a custom one if needed
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
