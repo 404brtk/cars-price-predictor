08ea3cf9d600bafc375f7a1fe99f11d288fc6140
Bartosz Zielonka <bzielonka1@proton.me>
2025-06-19 17:38:48 +0200

    add favicon and remove default svg and ico files

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import favicon from '../../public/favicon.ico';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
@@ -17,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'cars-price-predictor',
   description: 'predict the price of a used car in seconds.',
+  icons: {
+    icon: favicon.src,
+  },
 };
 
 export default function RootLayout({
